c084dd0d0e5b7523c035465287d6684b31966cf3
Adam Berger <adam.g.berger@gmail.com>
2019-01-09 23:48:10 -0500

    Added apache tika

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
                  [clj-http "3.9.1"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.async "0.4.490"]
-                 [org.apache.commons/commons-math3 "3.2"]]
+                 [org.apache.commons/commons-math3 "3.2"]
+                 [org.apache.tika/tika-core "1.20"]
+                 [org.apache.tika/tika-parsers "1.20"]]
   :main ^:skip-aot com.adamgberger.predictit.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
