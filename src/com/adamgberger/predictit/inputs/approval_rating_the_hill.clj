250876888a72390fd112efc064b5608828d60e7f
Adam Berger <adam.g.berger@gmail.com>
2019-03-03 22:24:55 -0500

    Re-wrote the hill approval rating input in a more reasonable, async style

--- src/com/adamgberger/predictit/inputs/approval_rating_the_hill.clj
+++ src/com/adamgberger/predictit/inputs/approval_rating_the_hill.clj
@@ -1,5 +1,6 @@
 (ns com.adamgberger.predictit.inputs.approval-rating-the-hill
   (:require [clojure.string :as string]
+            [clojure.core.async :as async]
             [clj-http.client :as h]
             [com.adamgberger.predictit.lib.utils :as u]
             [com.adamgberger.predictit.lib.log :as l])
@@ -8,13 +9,67 @@
 
 (def id ::id)
 
-(defn- current-from-sheet [stream cb err-cb]
+(defn- async-put-once [ch val]
+  (async/go
+    (async/>! ch val)
+    (async/close! ch)))
+
+(defn- async-retry
+  ([out-ch pipeline tries]
+    (async-retry out-ch pipeline tries tries 0))
+  ([out-ch pipeline orig-tries tries skip]
+    (let [ch (async/chan)]
+      (async/go-loop [i 0]
+        (if-let [val (async/<! ch)]
+          (if (and (instance? Exception val)
+                  (> tries 0))
+            (do (l/log :warn "Retrying" (l/ex-log-msg val))
+                (async/<! (async/timeout (+ 100 (rand) (* 200 (Math/pow 2 (inc (- orig-tries tries)))))))
+                (async-retry out-ch pipeline orig-tries (dec tries) i))
+            (do (when (>= i skip) (async/>! out-ch val))
+                (recur (inc i))))
+          (async/close! out-ch)))
+      (pipeline ch))))
+
+(defn- async-mapcat [f in-ch]
+  (let [out-ch (async/chan)]
+    (async/go-loop []
+      (if-let [x (async/<! in-ch)]
+        (let [vals (f x)]
+          (doseq [val vals]
+            (async/>! out-ch val))
+          (recur))
+        (async/close! out-ch)))
+    out-ch))
+
+(defn- async-filter [f in-ch]
+  (let [out-ch (async/chan)]
+    (async/go-loop []
+      (if-let [x (async/<! in-ch)]
+        (do
+          (when (f x) (async/>! out-ch x))
+          (recur))
+        (async/close! out-ch)))
+    out-ch))
+
+(defn- async-wrap-error [f]
+  (fn [val out-ch]
+    (if (instance? Exception val)
+      (async-put-once out-ch val)
+      (f val out-ch))))
+
+(defn- async-thread [f in-ch]
+  (let [c (async/chan)]
+    (async/pipeline-async 1 c f in-ch)
+    c))
+
+(defn- current-from-sheet [stream out-ch]
   (let [tika (Tika.)
         s (.parseToString tika stream)
         q "Do you approve or disapprove of the job Donald Trump is doing as President of the United States?"
         q-idx (string/index-of s q)]
     (if (nil? q-idx)
-        (cb nil)
+        (async/close! out-ch)
         (let [q-end-idx (+ 1 q-idx (count q))
               net-approve-str "NET APPROVE"
               net-approve-idx (string/index-of s net-approve-str q-end-idx)
@@ -24,44 +79,97 @@
               line (subs s (inc net-approve-line-end) net-approve-pct-line-end)
               [_ approval] (re-find #"\s*(\d+)%" line)
               {date :end-date} (u/parse-human-date-range-within s)]
-          (cb {:val (Integer/parseInt approval)
-              :date date
-              :next-expected (u/next-specific-weekday-at (java.time.LocalDate/now) u/ny-time 5 14 0)})))))
+          (async-put-once
+            out-ch
+            {:val (Integer/parseInt approval)
+             :date date
+             :next-expected (u/next-specific-weekday-at (java.time.LocalDate/now) u/ny-time 5 14 0)})))))
 
-(defn- current-from-article-html [html cb err-cb]
+(defn- url-stream [url out-ch]
+  (h/get
+    url
+    {:async? true
+      :as :stream}
+    #(async-put-once out-ch (:body %))
+    #(async-put-once out-ch (ex-info "Failed to get URL stream" {:url url} %))))
+
+(defn test-it [url]
+  (let [{stream :body :as stuff} (h/get url {:as :stream})
+        _ (println stuff)
+        ;_ (println "HERE" (.read (java.io.InputStreamReader. stream)) "YO")
+        ;stream (java.io.ByteArrayInputStream. (.getBytes body "UTF-8"))
+        tika (Tika.)
+        s (.parseToString tika stream)
+        q "Do you approve or disapprove of the job Donald Trump is doing as President of the United States?"
+        q-idx (string/index-of s q)]
+    (if (nil? q-idx)
+        {:nope "nope"}
+        (let [q-end-idx (+ 1 q-idx (count q))
+              net-approve-str "NET APPROVE"
+              net-approve-idx (string/index-of s net-approve-str q-end-idx)
+              net-approve-end-idx (+ 1 net-approve-idx (count net-approve-str))
+              net-approve-line-end (string/index-of s "\n" net-approve-end-idx)
+              net-approve-pct-line-end (string/index-of s "\n" (inc net-approve-line-end))
+              line (subs s (inc net-approve-line-end) net-approve-pct-line-end)
+              [_ approval] (re-find #"\s*(\d+)%" line)
+              {date :end-date} (u/parse-human-date-range-within s)]
+          {:val (Integer/parseInt approval)
+            :date date
+            :next-expected (u/next-specific-weekday-at (java.time.LocalDate/now) u/ny-time 5 14 0)}))))
+
+(defn- article-html-to-sheet-url [html out-ch]
   (let [regex #"<iframe src=\"(https://onedrive.live.com/embed?[^\"]+)\""
         [_ sheet-url] (re-find regex html)]
-    (when (some? sheet-url)
-      (h/get
+    (if (some? sheet-url)
+      (async-put-once
+        out-ch
         (-> sheet-url
             (string/replace "/embed?" "/download?")
-            (string/replace "&amp;" "&"))
-        {:async? true
-         :as :stream}
-        #(current-from-sheet (:body %) cb err-cb)
-        err-cb))))
+            (string/replace "&amp;" "&")))
+      (async/close! out-ch))))
 
-(defn- current-from-article [url cb err-cb]
+(defn- article-html [url out-ch]
   (h/get
-    (str "https://thehill.com" url)
+    url
     {:async? true}
-    #(current-from-article-html (:body %) cb err-cb)
-    err-cb))
+    #(async-put-once out-ch (:body %))
+    #(async-put-once out-ch (ex-info "Failed to get article html" {:url url} %))))
 
-(defn- current-from-articles [html cb err-cb]
+(defn- get-articles [html]
   (let [regex #"\"(/hilltv/what-americas-thinking/[^\"]+)\""
-        matches (re-seq regex html)]
-    (doseq [[_ url] (take 10 matches)]
-      (current-from-article
-        url
-        #(when (some? %) (cb %))
-        err-cb))))
+        matches (re-seq regex html)
+        article-urls (->> matches
+                          (map second)
+                          distinct
+                          (map (partial str "https://thehill.com")))]
+    article-urls))
 
-
+(defn- sheet-url-to-approval-val [url out-ch]
+  (async-retry
+    out-ch
+    (fn [ch]
+      (->> (async/to-chan [url])
+           (async-thread (async-wrap-error url-stream))
+           (async-thread (async-wrap-error current-from-sheet))
+           (#(async/pipe % ch))))
+    0))
+ 
 (defn get-current [cb]
-  (let [err-cb #(l/log :error "Failed to get the hill poll" (l/ex-log-msg %))]
+  (let [html-chan (async/chan)
+        err-cb #(l/log :error "Failed to get the hill poll" (l/ex-log-msg %))]
     (h/get
       "https://thehill.com/hilltv/america"
       {:async? true}
-      #(current-from-articles (:body %) cb err-cb)
-      err-cb)))
+      #(async/go (async/>! html-chan (:body %))
+                 (async/close! html-chan))
+      err-cb)
+    (->> html-chan
+        (async-mapcat get-articles)
+        (async-thread (async-wrap-error article-html))
+        (async-thread (async-wrap-error article-html-to-sheet-url))
+        (async-thread (async-wrap-error sheet-url-to-approval-val))
+        (async/take 1)
+        ; TODO: add a timeout for success and failure case
+        (async-thread (async-wrap-error #(do (cb %1) (async/close! %2))))
+        (async-thread #(do (l/log :error "Error in the hill approval rating" (l/ex-log-msg %1))
+                           (async/close! %2))))))
