9e3be73a0c0ee86caa2c010308bf94ff20f41377
Adam Berger <adam.g.berger@gmail.com>
2019-09-08 16:22:59 -0400

    Refactored out most of the indexing logic

--- src/com/adamgberger/predictit/lib/multi_contract_portfolio_utils.clj
+++ src/com/adamgberger/predictit/lib/multi_contract_portfolio_utils.clj
@@ -26,17 +26,62 @@
 (def ^:private round2 (partial round 2))
 (def ^:private round4 (partial round 4))
 
+(defn- all-events [contracts]
+  (map-indexed
+    (fn [idx contract]
+      {:idx idx
+       :contract-idx idx
+       :contract contract})
+    contracts))
+
+(defn- cash-contracts [contracts]
+  [{:idx 0
+    :contract-idx -1
+    :contract :cash}])
+
+(defn- yes-contracts [contracts]
+  (let [last-cash (->> contracts cash-contracts last :idx)]
+    (map-indexed
+      (fn [idx contract]
+        {:idx (+ last-cash 1 idx)
+         :contract-idx idx
+         :contract contract})
+      contracts)))
+
+(defn- no-contracts [contracts]
+  (let [last-yes (->> contracts yes-contracts last :idx)]
+    (map-indexed
+      (fn [idx contract]
+        {:idx (+ last-yes 1 idx)
+         :contract-idx idx
+         :contract contract})
+      contracts)))
+
+(defn- all-contracts [contracts]
+  (concat (cash-contracts contracts)
+          (yes-contracts contracts)
+          (no-contracts contracts)))
+
+(defn- events-by-contracts [contracts f]
+  (let [events (all-events contracts)
+        cash-bets (cash-contracts contracts)
+        yes-bets (yes-contracts contracts)
+        no-bets (no-contracts contracts)
+        e-by-c (make-array Double/TYPE (count events) (apply + (map count [cash-bets yes-bets no-bets])))]
+    (doseq [{event :idx} events]
+      (doseq [{:keys [idx contract]} cash-bets]
+        (aset e-by-c event idx (f event contract :yes :winner)))
+      (doseq [{:keys [idx contract-idx contract]} yes-bets]
+        (aset e-by-c event idx (f event contract :yes (if (= contract-idx event) :winner :loser))))
+      (doseq [{:keys [idx contract-idx contract]} no-bets]
+        (aset e-by-c event idx (f event contract :no (if (= contract-idx event) :loser :winner)))))
+    e-by-c))
+
 (defn- winner-matrix [contracts]
-  (let [num-events (count contracts)
-        num-bets (inc (* 2 num-events))
-        M (make-array Double/TYPE num-events num-bets)]
-    (doseq [event (range num-events)]
-      (aset M event 0 1) ; cash
-      (doseq [yes (range num-events)]
-        (aset M event (inc yes) (if (= yes event) 1.0 0.0001))) ; yes bet on event is a winner, other yes's lose
-      (doseq [no (range num-events)]
-        (aset M event (+ num-events no 1) (if (= no event) 0.0001 1)))) ; our no loses, other nos win
-      M))
+  (events-by-contracts
+    contracts
+    (fn [_ _ _ wl]
+      (if (= wl :winner) 1.0 0.0001))))
 
 (defn- prob-vector [contracts]
   (double-array (map :prob-yes contracts)))
@@ -47,45 +92,37 @@
     (/ (- 1.0 (* commission-rate gross-win)) price)))
 
 (defn- odds-matrix [contracts]
-  (let [num-events (count contracts)
-        num-bets (inc (* 2 num-events))
-        o (make-array Double/TYPE num-events num-bets)]
-    (doseq [event (range num-events)
-            :let [{:keys [price-yes]} (get contracts event)]]
-      ; cash returns 1x
-      (aset o event 0 1)
-      ; our yes returns our odds and all other yes's return 0
-      (doseq [e (range num-events)]
-        (aset o event (inc e) (if (= e event) (odds-with-commission price-yes) 0.0001)))
-      ; our no returns 0 and all no's other than us return their odds
-      (doseq [no (range num-events)
-              :let [{:keys [price-no]} (get contracts no)]]
-        (aset o event (+ num-events no 1) (if (= no event) 0.0001 (odds-with-commission price-no)))))
-    o))
+  (events-by-contracts
+    contracts
+    (fn [e c yn wl]
+      (cond
+        (= c :cash) 1.0 ; cash returns 1x
+        (= wl :loser) 0.0001 ; losers return 0x
+        :else (odds-with-commission (get c ({:yes :price-yes :no :price-no} yn))))))) ; winners return their odds
 
 (defn- objective-fn [contracts]
-  (let [num-events (count contracts)
-        num-bets (inc (* 2 num-events))]
-    (->> contracts
-         (map-indexed
-           (fn [i {:keys [prob]}]
-             ["p(" i ") * ln("
-              (->> num-bets
-                   range
-                   (map
-                     (fn [j] (str "M(" i ", " j ") * x(" j ") * o(" i ", " j ")")))
-                   (interpose " + "))
-              ")"]))
-         (interpose " + ")
-         flatten
-         (apply str))))
+  (->> contracts
+       all-events
+       (map
+         (fn [{event-idx :idx}]
+           ["p(" event-idx ") * ln("
+            (->> contracts
+                 all-contracts
+                 (map
+                   (fn [{bet-idx :idx}]
+                     (str "M(" event-idx ", " bet-idx ") * x(" bet-idx ") * o(" event-idx ", " bet-idx ")")))
+                 (interpose " + "))
+            ")"]))
+       (interpose " + ")
+       flatten
+       (apply str)))
 
 (defn- constraints [contracts]
-  (let [var-for-idx #(str "x(" % ")")
+  (let [to-var (fn [{:keys [idx]}] (str "x(" idx ")"))
         n-contracts (count contracts)
-        vars (->> n-contracts (* 2) inc range (map var-for-idx))
-        yes-vars (->> n-contracts inc (range 1) (map var-for-idx))
-        no-vars (->> n-contracts (* 2) inc (range (inc n-contracts)) (map var-for-idx))]
+        vars (->> contracts all-contracts (map to-var))
+        yes-vars (->> contracts yes-contracts (map to-var))
+        no-vars (->> contracts no-contracts (map to-var))]
     (->> vars
          (mapcat
            #(vector
